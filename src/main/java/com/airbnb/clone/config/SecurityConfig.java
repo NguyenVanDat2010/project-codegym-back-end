d6ce1ac14d347ae8da9539bfbd8f919786a7554c
Long Nguyen <n.h.long.9697@gmail.com>
2020-09-01 11:30:48 +0700

    fix mapper id and response message bug

--- src/main/java/com/airbnb/clone/config/SecurityConfig.java
+++ src/main/java/com/airbnb/clone/config/SecurityConfig.java
@@ -37,6 +37,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/auth/**")
                 .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/houses/")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/houses/**")
+                .permitAll()
                 .antMatchers("/v2/api-docs",
                         "/configuration/ui",
                         "/swagger-resources/**",
---
+++ src/main/java/com/airbnb/clone/dto/ResponseMessage.java
@@ -0,0 +1,20 @@
+package com.airbnb.clone.dto;
+
+public class ResponseMessage {
+    private String message;
+
+    public ResponseMessage(String message) {
+        this.message = message;
+    }
+
+    public ResponseMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
